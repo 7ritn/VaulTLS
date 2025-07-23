49d34f4b5e6b69b15813cf1605a514230fdfd41c
Emily Ehlert <ehlertto@hu-berlin.de>
2025-07-23 12:12:26 +0200

    Make argon2 easy for testing

--- backend/src/constants.rs
+++ backend/src/constants.rs
@@ -8,9 +8,17 @@ pub(crate) const CA_FILE_PATH: &str = "ca.cert";
 pub(crate) const API_PORT: u16 = 3737;
 pub const VAULTLS_VERSION: &str = "v0.8.0";
 
+#[cfg(not(test))]
 pub static ARGON2: Lazy<Argon2<'static>> = Lazy::new(|| {
     let params = Params::new(64 * 1024, 3, 4, Some(50))
     .expect("Failed to create Argon2 parameters");
 
+    Argon2::new(Algorithm::Argon2id, Version::V0x13, params)
+});
+
+#[cfg(test)]
+pub static ARGON2: Lazy<Argon2<'static>> = Lazy::new(|| {
+    // Test setup (weaker params for speed)
+    let params = Params::new(1024, 1, 1, Some(50)).unwrap();
     Argon2::new(Algorithm::Argon2id, Version::V0x13, params)
 });
